a9bf4330a5664d6332244408d7a4da790f93209f
GondolaAdventures <83150401+GondolaAdventures@users.noreply.github.com>
2022-09-29 18:51:55 +0800

    php MySQL

---
+++ accreg.php
@@ -0,0 +1,45 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if (!empty($email) || !empty($password)) {
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "labibliodb";
+
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+    if (mysqli_connect_error()) {
+        die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+    } else {
+        $SELECT = "SELECT email From accounts Where email = ? Limit 1";
+        $INSERT = "INSERT Into accounts (email, password) values(?, ?)";
+
+        //Prepare statement
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
+        
+        if ($rnum==0) {
+            $stmt->close();
+
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("ssssii", $email, $password);
+            $stmt->execute();
+            echo "New record inserted successfully";
+        } else {
+            echo "Someone already register using this email";
+        }
+        $stmt->close();
+        $conn->close();
+    }
+} else {
+    echo "All field are required";
+    die();
+}
+?>
