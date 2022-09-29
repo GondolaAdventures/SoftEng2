ae0a32ab5d9fea7160fa1fcb238059d34778aace
TanniKing <83150392+TanniKing@users.noreply.github.com>
2022-09-29 17:04:59 +0800

    Files modal completely renovated
    has "choose file" button now, however importing of files isnt implemented yet

--- index.js
+++ index.js
@@ -9,3 +9,8 @@
                     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
                 });
             });
+
+// clears the file field when called
+function deleteFile() {
+    document.getElementById('formFileMultiple').value = "";
+    }
