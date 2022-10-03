1b27a755e3e58daaeeba1fb0b79e747873c84260
GondolaAdventures <83150401+GondolaAdventures@users.noreply.github.com>
2022-10-03 20:20:57 +0800

    acclog bug

--- acclog.php
+++ acclog.php
@@ -14,31 +14,29 @@ if (!empty($email) || !empty($password)) {
     if (mysqli_connect_error()) {
         die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
     } else {
-        //$SELECT = "SELECT email From accounts Where email = ? Limit 1";
-        $SELECT2 = "SELECT From accounts (email, password)";
+        $SELECT = "SELECT * from accounts WHERE email = ?";
 
         //Prepare statement
-        // $stmt = $conn->prepare($SELECT);
-        // $stmt->bind_param("s", $email);
-        // $stmt->execute();
-        // $stmt->bind_result($email);
-        // $stmt->store_result();
-        // $rnum = $stmt->num_rows;
-
-        $stmt2 = $conn->prepare($SELECT2);
-        $stmt2->bind_param("ss", $email, $password);
-        $stmt2->execute();
-        $stmt2->bind_result($email, $password);
-        $stmt2->store_result();
-        $rnum2 = $stmt2->num_rows;
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
         
-        if ($rnum2==0) {
+        if ($rnum==0) {
             header('Location: /SoftEng2/logIn.html');
         } else {
+            $data = $stmt->fetch_assoc();
+            if (password_verify($password, $data['password'])) {
+                header('Location: /SoftEng2/index.html');
+            } else {
+                header('Location: /SoftEng2/logIn.html');
+            }
             header('Location: /SoftEng2/index.html');
         }
         // $stmt->close();
-        $stmt2->close();
+        $stmt->close();
         $conn->close();
     }
 } else {
